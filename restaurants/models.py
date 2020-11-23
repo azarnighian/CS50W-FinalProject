8d48285da11cb9d51d9e7b825ee18b1bf6d03fb2
Aharon <aharonzarnighian@gmail.com>
2020-11-23 13:40:56 -0500

    Setup complete

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+# After changing anything here:
+    # python3 manage.py makemigrations
+    # python3 manage.py migrate
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse # DELETE AFTERWARDS
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "restaurants/index.html") 
