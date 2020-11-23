c79acee5829d1195217a8b6f7bd97faca6877cbf
Aharon <aharonzarnighian@gmail.com>
2020-11-23 14:40:44 -0500

    gitignore update

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v9pb!&yma^o&*g00%#sc+v@m8&hj(rmdl3j1ky09shkae8_#x*'
+with open('/Users/azarnighian/Desktop/CS50W/Final Project/capstone/finalproject/finalproject/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
