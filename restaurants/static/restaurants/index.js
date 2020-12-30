27502f678847b37144f2789f3676508765d86dc1
Aharon <aharonzarnighian@gmail.com>
2020-12-30 11:23:35 -0500

    Added filters bar to mobile

--- restaurants/static/restaurants/index.js
+++ restaurants/static/restaurants/index.js
@@ -6,6 +6,8 @@
 
 document.addEventListener('DOMContentLoaded', function () {    
     navSlide();
+
+    document.querySelector('.filters-bar').addEventListener('click', () => filters_bar());
     
     // document.querySelector('.testing_button').onclick = tester;    
 
@@ -17,76 +19,6 @@ document.addEventListener('DOMContentLoaded', function () {
     // };
 });
 
-// Geocoding
-function geocodeAddress(geocoder, callback) {
-    address = document.getElementById("id_city").value;
-    geocoder.geocode({ address: address }, (results, status) => {
-        if (status === "OK") {
-            callback(results);
-        } else {
-            alert("Geocode was not successful for the following reason: " + status);
-        }
-    });
-}
-
-function geo_callback(results) {
-    coordinates = [results[0].geometry.location.lat(), results[0].geometry.location.lng()]
-    search(coordinates);
-}
-
-
-// Maps Javascript API
-
-var service;
-
-function search(coordinates) {           
-    var city = new google.maps.LatLng(coordinates[0], coordinates[1]);
-
-    var request = {
-        location: city,
-        radius: '3000',
-        type: ['restaurant']
-    };
-
-    service = new google.maps.places.PlacesService(document.createElement('div'));
-    service.nearbySearch(request, search_callback);
-}
-
-function search_callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-        send_to_view(results);
-    }
-}
-
-function tester() {
-    const data = { username: 'example' };
-
-    fetch('/testing', {
-        method: 'POST',        
-        body: JSON.stringify(data)
-    })          
-}
-
-function send_to_view(results) {
-    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-    const data = { username: 'example' };
-    fetch(`/results/${address}`, {
-        method: 'POST',
-        mode: 'same-origin',
-        headers: { 
-            'X-CSRFToken': csrftoken,
-            'Content-Type': 'application/json'                        
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
-}
 
 // Nav Bar
 const navSlide = () => {
@@ -110,3 +42,86 @@ const navSlide = () => {
         burger.classList.toggle('toggle');
     });        
 }
+
+function filters_bar() {
+    if (document.querySelector('.sidebar-container').style.display == 'none') {
+        document.querySelector('.sidebar-container').style.display = 'block';
+        document.querySelector('.sidebar-and-cards-container').style.width = '100%';    
+        document.querySelector('.cards-container').style.display = 'none';
+    }  
+    else {
+        document.querySelector('.sidebar-container').style.display = 'none';
+        document.querySelector('.cards-container').style.display = 'flex';
+    }      
+}
+
+// Geocoding
+// function geocodeAddress(geocoder, callback) {
+//     address = document.getElementById("id_city").value;
+//     geocoder.geocode({ address: address }, (results, status) => {
+//         if (status === "OK") {
+//             callback(results);
+//         } else {
+//             alert("Geocode was not successful for the following reason: " + status);
+//         }
+//     });
+// }
+
+// function geo_callback(results) {
+//     coordinates = [results[0].geometry.location.lat(), results[0].geometry.location.lng()]
+//     search(coordinates);
+// }
+
+
+// // Maps Javascript API
+
+// var service;
+
+// function search(coordinates) {           
+//     var city = new google.maps.LatLng(coordinates[0], coordinates[1]);
+
+//     var request = {
+//         location: city,
+//         radius: '3000',
+//         type: ['restaurant']
+//     };
+
+//     service = new google.maps.places.PlacesService(document.createElement('div'));
+//     service.nearbySearch(request, search_callback);
+// }
+
+// function search_callback(results, status) {
+//     if (status == google.maps.places.PlacesServiceStatus.OK) {
+//         send_to_view(results);
+//     }
+// }
+
+// function tester() {
+//     const data = { username: 'example' };
+
+//     fetch('/testing', {
+//         method: 'POST',        
+//         body: JSON.stringify(data)
+//     })          
+// }
+
+// function send_to_view(results) {
+//     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+//     const data = { username: 'example' };
+//     fetch(`/results/${address}`, {
+//         method: 'POST',
+//         mode: 'same-origin',
+//         headers: { 
+//             'X-CSRFToken': csrftoken,
+//             'Content-Type': 'application/json'                        
+//         },
+//         body: JSON.stringify(data)
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log('Success:', data);
+//     })
+//     .catch((error) => {
+//         console.error('Error:', error);
+//     });
+// }
