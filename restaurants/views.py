a67ca17c54235c92c27c9b5cb5ae45fca1c5354b
Aharon <aharonzarnighian@gmail.com>
2021-01-27 12:58:30 -0500

    Removed Google Places API stuff, and added hover effects to submit buttons

--- restaurants/views.py
+++ restaurants/views.py
@@ -9,10 +9,6 @@ from django.views.decorators.csrf import csrf_exempt
 
 from .models import User
 
-import googlemaps
-
-from shutil import copy
-import os 
 
 class SearchForm(forms.Form):
     city = forms.CharField(label="", 
@@ -95,108 +91,19 @@ def results(request, city, filters, radius, keyword, min_price, max_price):
                 "form": form
             })
 
-    results = search(city, filters, radius, keyword, min_price, max_price)
-
-    results_details = []
-    for result in results:
-        results_details.append(restaurant_details(result["place_id"]))
-
+    
     # Get API key
-    with open('/Users/azarnighian/Desktop/CS50W/Final Project/capstone/finalproject/finalproject/api_key.txt') as file:
-        api_key = file.read().strip()
+    # with open('/Users/azarnighian/Desktop/CS50W/Final Project/capstone/finalproject/finalproject/api_key.txt') as file:
+    #     api_key = file.read().strip()
 
-    if keyword == "empty":
-        return render(request, "restaurants/results.html", {
-            "city": city,             
-            "restaurants": results_details,
-            "api_key": api_key,
-            "form": SidebarForm(initial={'location': city, 'radius': radius, 
-                                        'min_price': min_price, 
-                                        'max_price': max_price})                                                  
-        })      
-    else:
-        return render(request, "restaurants/results.html", {
-            "city": city,            
-            "restaurants": results_details,
-            "api_key": api_key,
-            "form": SidebarForm(initial={'location': city, 'radius': radius, 
-                                        'keyword': keyword, 'min_price': min_price, 
-                                        'max_price': max_price})
-        })                                                   
-
-
-def search(city, filters, radius, keyword, min_price, max_price):
-    # Get API key
-    with open('/Users/azarnighian/Desktop/CS50W/Final Project/capstone/finalproject/finalproject/api_key.txt') as file:
-        api_key = file.read().strip()
-
-    # https://github.com/googlemaps/google-maps-services-python
-    # https://www.youtube.com/watch?v=qkSmuquMueA
-    
-    gmaps = googlemaps.Client(key=api_key)
-    
-    # Geocode city 
-    geocode_result = gmaps.geocode(city)
-    lat_lng = geocode_result[0]["geometry"]["location"]
-    lat = lat_lng["lat"]
-    lng = lat_lng["lng"]    
-
-    if filters == "no":
-        # Search for restaurants nearby city    
-        places_result = gmaps.places_nearby(type="restaurant", 
-                        location=[lat,lng], radius=radius)
-    else:
-        # Search for restaurants nearby city, with filters    
-        places_result = gmaps.places_nearby(type="restaurant", location=[lat,lng], 
-                        radius=radius, keyword=keyword, min_price=min_price,
-                        max_price=max_price)                        
-
-    # To save photos
-    # get_photos(gmaps, places_result)
-
-    return places_result["results"]
-
-
-def get_photos(gmaps, result):        
-    counter = 1
-
-    for place in result['results']:
-        if 'photos' in place:            
-            photo_reference_string = place['photos'][0]['photo_reference']
-            raw_image_data = gmaps.places_photo(photo_reference_string, max_width=300, max_height=300)        
-            
-            f = open(f'restaurants/static/restaurants/Restaurant_Images/Restaurant{counter}.jpg', 'wb')        
-            for chunk in raw_image_data:
-                if chunk:
-                    f.write(chunk)
-            f.close()
-
-            counter += 1
-        else:
-            # https://stackoverflow.com/questions/123198/how-do-i-copy-a-file-in-python
-            # https://thispointer.com/python-how-to-copy-files-from-one-location-to-another-using-shutil-copy/
-            copy('restaurants/static/restaurants/unavailable-circle.png', 'restaurants/static/restaurants/Restaurant_Images')
-
-            # https://www.geeksforgeeks.org/python-os-rename-method/
-            os.rename('restaurants/static/restaurants/Restaurant_Images/unavailable-circle.png', f'restaurants/static/restaurants/Restaurant_Images/Restaurant{counter}.jpg')
-
-            counter += 1
-
-
-def restaurant_details(id):
-    with open('/Users/azarnighian/Desktop/CS50W/Final Project/capstone/finalproject/finalproject/api_key.txt') as file:
-        api_key = file.read().strip()
-
-    gmaps = googlemaps.Client(key=api_key)
-
-    my_fields = ['formatted_address', 'name', 'photo', 'place_id', 'formatted_phone_number', 'opening_hours', 'website']
-    
-    details_result = gmaps.place(place_id=id, fields=my_fields)
-
-    return details_result["result"]
-
-    # To get photos:
-    # get_photos(gmaps, details_result)     
+    return render(request, "restaurants/results.html", {
+        "city": city,            
+        "restaurants": results_details,
+        "api_key": api_key,
+        "form": SidebarForm(initial={'location': city, 'radius': radius, 
+                                    'keyword': keyword, 'min_price': min_price, 
+                                    'max_price': max_price})
+    })                                                        
 
 
 def restaurant_page(request, name, id):
