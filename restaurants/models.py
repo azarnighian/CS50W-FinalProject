0c958cad8af19e387a8f6a0df60149d3293ca24b
Aharon <aharonzarnighian@gmail.com>
2021-02-17 14:08:11 -0500

    Added Restaurant model and saved_restaurants field

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+# https://www.reddit.com/r/django/comments/llb98f/arraylist_as_model_field/
+class Restaurant(models.Model):
+    name = models.CharField(max_length=500)
+    regular_id = models.CharField(max_length=500)
+    details_id = models.CharField(max_length=500)
+
 # https://docs.djangoproject.com/en/3.0/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
 class User(AbstractUser):
-    pass
-
+    saved_restaurants = models.ManyToManyField(Restaurant, blank=True)
 
 
 # After changing anything here:
