77f3ee8e8992c32d102957520313886fb0838de0
Aharon <aharonzarnighian@gmail.com>
2021-04-12 13:34:29 -0400

    Added progress cursor (buggy though)

---
+++ restaurants/static/restaurants/progress_cursor.js
@@ -0,0 +1,19 @@
+// Change cursor to progress on submit
+
+// Stopped using this because of the following issue:
+    // https://stackoverflow.com/questions/1718415/getting-the-browser-cursor-from-wait-to-auto-without-the-user-moving-the-mou/37351569#37351569
+    // https://stackoverflow.com/questions/36597412/mouse-cursor-set-using-jquery-css-not-changing-until-mouse-moved
+
+document.addEventListener('DOMContentLoaded', function () {
+    var form = document.querySelector('form');
+    var submitButton = document.querySelector('input[type=submit]');
+
+    // (For when the page reloads after filters form submission)
+    // submitButton.style.cursor = 'alias';
+    // document.body.style.cursor = 'alias';
+
+    form.addEventListener('submit', function () {
+        submitButton.style.cursor = 'progress';
+        document.body.style.cursor = 'progress';        
+    });
+});
