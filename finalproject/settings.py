0ebbb1f228ca71cdddadf3839291e7d76f47a7c5
Aharon <aharonzarnighian@gmail.com>
2021-04-13 14:35:21 -0400

    Production stuff

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -22,8 +22,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 with open('/Users/azarnighian/Desktop/CS50W/Final Project/finalproject/finalproject/secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
 
+# PRODUCTION
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
+
+# PRODUCTION
+# https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/#csrf-cookie-secure
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = []
 
@@ -42,6 +49,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [    
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+        # PRODUCTION^
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,4 +128,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+    # PRODUCTION^
+
+# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
+
+# PRODUCTION:
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# PRODUCTION:
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
