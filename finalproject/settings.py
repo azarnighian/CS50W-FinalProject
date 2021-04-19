dd273e398ce58c8a4d99a6b5811c2a76d3ec7989
Aharon <aharonzarnighian@gmail.com>
2021-04-19 14:49:19 -0400

    Changed STATIC_ROOT in setings

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -190,7 +190,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
     # PRODUCTION^
 
 # The URL to use when referring to static files (where they will be served from)
