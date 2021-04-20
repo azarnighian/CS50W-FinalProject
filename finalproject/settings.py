8c743feb7599560b0e470e08cc7d5025676b56c2
Aharon <aharonzarnighian@gmail.com>
2021-04-20 11:09:19 -0400

    Templates DIRS in settings

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -125,7 +125,8 @@ ROOT_URLCONF = 'finalproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS':  [os.path.join(BASE_DIR, 'templates')],
+            # PRODUCTION^
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
