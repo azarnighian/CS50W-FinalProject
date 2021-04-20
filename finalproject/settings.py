a89995f8111586a078c24ce7ca98f292a7717d70
Aharon <aharonzarnighian@gmail.com>
2021-04-20 16:10:51 -0400

    Changed DEBUG yet again

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -46,7 +46,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # DEBUG = True
 # DEBUG = False
 # https://www.reddit.com/r/django/comments/kiphpl/django_deployment_heroku_server_status_500_when/gu8idla?utm_source=share&utm_medium=web2x&context=3
-DEBUG = os.environ.get('DEBUG')
+DEBUG = env("DEBUG")
 
 # PRODUCTION:
 # https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/#csrf-cookie-secure
