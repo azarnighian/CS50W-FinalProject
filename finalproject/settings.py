1829e893babc6471125d25436c8ce8918787d5b7
Aharon <aharonzarnighian@gmail.com>
2021-04-20 10:22:10 -0400

    Removed STATICFILES_DIRS from settings

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -197,12 +197,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # PRODUCTION:
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-# PRODUCTION:
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
