84313a5936638187fad882b6361cd6495a26a241
Aharon <aharonzarnighian@gmail.com>
2021-04-20 13:29:56 -0400

    Temporarily changed website name for heroku

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -66,8 +66,8 @@ EMAIL_PORT = 587
 
 # PRODUCTION:
 # ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['trovare1.herokuapp.com', '127.0.0.1']
-ALLOWED_HOSTS = ['trovare1.herokuapp.com']
+ALLOWED_HOSTS = ['discoverrestaurants.herokuapp.com', '127.0.0.1']
+# ALLOWED_HOSTS = ['trovare1.herokuapp.com']
 # ALLOWED_HOSTS = ['*']
 
 # PRODUCTION:
