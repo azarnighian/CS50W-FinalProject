af598add11d3576a2f6d8164ffb441654bb53630
Aharon <aharonzarnighian@gmail.com>
2021-04-27 14:16:44 -0400

    Changed NavBarHeight from vh to rem

--- restaurants/static/restaurants/navSlide.js
+++ restaurants/static/restaurants/navSlide.js
@@ -1,18 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {    
+document.addEventListener('DOMContentLoaded', function () {                    
     // Fix for vh units on mobile:
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-    
-    // Do the same thing if screen gets resized, like if device gets rotated
-    window.addEventListener('resize', () => {
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });        
-    
-    // Same thing but just 100vh
     let vh100 = window.innerHeight;
     document.documentElement.style.setProperty('--vh100', `${vh100}px`);            
     
+    // Do the same thing if screen gets resized, like if device gets rotated
     window.addEventListener('resize', () => {
         let vh100 = window.innerHeight;
         document.documentElement.style.setProperty('--vh100', `${vh100}px`);        
